df119421b9b5333d4820086d16a4204861c39c77
ckoshien <ckoshien@gmail.com>
2022-11-13 23:48:28 +0900

    no message

--- run.py
+++ run.py
@@ -31,15 +31,20 @@ try:
         if tdatetime + timedelta(days=2) > dt.now():
             print(game)
             # 255: clear the frame
-            firstPosition = 640/3 - len(game['first_team_name'])/2*24
-            lastPosition = 640*3/4 - len(game['last_team_name'])/2*24
+            firstPosition = 640/3 - (len(game['first_team_name'])/2 - 1)*24
+            lastPosition = 640*3/4 - (len(game['last_team_name'])/2 - 1)*24
             Himage = Image.new('1', (epd.width, epd.height), 255)
             draw = ImageDraw.Draw(Himage)
             draw.text((10, 0), game['name'], font = jaFontSmall, fill = 0)
             draw.text((firstPosition, 60), game['first_team_name'], font = jaFontSmall, fill = 0)
             draw.text((lastPosition, 60), game['last_team_name'], font = jaFontSmall, fill = 0)
             draw.text((640/3, 120), str(game['first_run']), font = enFont, fill = 0)
+            draw.text((640/2 - 48, 120), '-', font = enFont, fill = 0)
             draw.text((640*3/4, 120), str(game['last_run']), font = enFont, fill = 0)
+            draw.text((firstPosition, 200), '勝:' + game['winner'], font = jaFontSmall, fill = 0)
+            draw.text((firstPosition, 250), 'S:' + game['saver'], font = jaFontSmall, fill = 0)
+            draw.text((lastPosition, 250), 'H:' + game['holder'], font = jaFontSmall, fill = 0)
+            draw.text((firstPosition, 300), '敗:' + game['loser'], font = jaFontSmall, fill = 0)
             epd.display(epd.getbuffer(Himage))
             time.sleep(5)  
     
