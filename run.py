ada3e502680bfa9a0c9333ac656493a0043836f5
ckoshien <ckoshien@gmail.com>
2022-11-13 23:17:08 +0900

    no message

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ try:
     
     jaFontSmall = ImageFont.truetype('font/BIZUDGothic-Regular.ttf', 24)
     jaFont = ImageFont.truetype('font/BIZUDGothic-Regular.ttf', 48)
-    enFont = ImageFont.truetype('font/BebasNeue-Regular.ttf', 100)
+    enFont = ImageFont.truetype('font/BebasNeue-Regular.ttf', 110)
     api_url = 'https://jcbl-score.com/scoresheet/api/v1/game'
     response = requests.get(api_url)
     games = json.loads(response.text)
@@ -34,8 +34,8 @@ try:
             Himage = Image.new('1', (epd.width, epd.height), 255)
             draw = ImageDraw.Draw(Himage)
             draw.text((10, 0), game['name'], font = jaFontSmall, fill = 0)
-            draw.text((640/5, 60), game['first_team_name'], font = jaFont, fill = 0)
-            draw.text((640*4/5, 60), game['last_team_name'], font = jaFont, fill = 0)
+            draw.text((640/5, 60), game['first_team_name'], font = jaFontSmall, fill = 0)
+            draw.text((640*3/5, 60), game['last_team_name'], font = jaFontSmall, fill = 0)
             draw.text((640/3, 120), str(game['first_run']), font = enFont, fill = 0)
             draw.text((640*2/3, 120), str(game['last_run']), font = enFont, fill = 0)
             epd.display(epd.getbuffer(Himage))
