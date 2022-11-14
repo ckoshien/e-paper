90fd5481b572bab2a81d5053c5bea733304ad340
ckoshien <ckoshien@gmail.com>
2022-11-14 12:14:02 +0900

    no message

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from PIL import Image,ImageDraw,ImageFont
 import requests
 import json
 from datetime import datetime as dt, timedelta
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -25,43 +26,41 @@ try:
     api_url = 'https://jcbl-score.com/scoresheet/api/v1/game'
     response = requests.get(api_url)
     games = json.loads(response.text)
-    slicedGames = list(filter(
+    filteredGames = list(filter(
         lambda game: 
             dt.strptime(game['updated_at'], '%Y-%m-%d %H:%M:%S')
-            + timedelta(days=2) > dt.now()
+            + timedelta(days=7) > dt.now()
             , 
         games))
-    # 画面クリア
-    Himage = Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(Himage)
-    for i, game in enumerate(slicedGames[0:4]):
-        #tdatetime = dt.strptime(game['updated_at'], '%Y-%m-%d %H:%M:%S')
-        #if tdatetime + timedelta(minutes=15) > dt.now():
-        #if tdatetime + timedelta(days=2) > dt.now():
-        print(game)
-        # 255: clear the frame
-        firstPosition = width*2/7 - (len(game['first_team_name'])/2 - 1)*24
-        lastPosition = width*5/7 - (len(game['last_team_name'])/2 - 1)*24
-        draw.text((10, 0 + 200*i), game['name'], font = jaFontSmall, fill = 0)
-        draw.text((width - 10*16, 0 + 200*i), game['updated_at'], font = jaFontSmall, fill = 0)
-        draw.text((firstPosition, 30 + 200*i), game['first_team_name'], font = jaFontSmall, fill = 0)
-        draw.text((lastPosition, 30 + 200*i), game['last_team_name'], font = jaFontSmall, fill = 0)
-        draw.text((width*2/7, 60 + 200*i), str(game['first_run']), font = enFont, fill = 0)
-        draw.text((width/2 + 60, 60 + 200*i), '-', font = enFont, fill = 0)
-        draw.text((width*5/7, 60 + 200*i), str(game['last_run']), font = enFont, fill = 0)
-        if game['winner']:
-            draw.text((firstPosition, 150 + 200*i), '勝:' + game['winner'], font = jaFontSmall, fill = 0)
-        if game['saver']:
-            draw.text((lastPosition, 150 + 200*i), 'S:' + game['saver'], font = jaFontSmall, fill = 0)
-        if game['loser']:
-            draw.text((firstPosition, 175 + 200*i), '敗:' + game['loser'], font = jaFontSmall, fill = 0)
-        if game['holder']:
-            draw.text((lastPosition, 175 + 200*i), 'H:' + game['holder'], font = jaFontSmall, fill = 0)
-        if game['homer']:
-            draw.text((firstPosition, 200 + 200*i), 'HR:' + game['homer'], font = jaFontSmall, fill = 0)
-        draw.line((0, 200 + 200*i , 448, 200 + 200*i), fill = 0)
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(5)  
+    for j in range(Decimal.quantize(len(filteredGames)/4,rounding=ROUND_HALF_UP)):
+        # 画面クリア
+        Himage = Image.new('1', (epd.height, epd.width), 255)
+        draw = ImageDraw.Draw(Himage)
+        slicedGames = filteredGames[j*4:(j+1)*4]
+        for i, game in enumerate(slicedGames):
+            print(game)
+            firstPosition = width*2/7 - (len(game['first_team_name'])/2 - 1)*24
+            lastPosition = width*5/7 - (len(game['last_team_name'])/2 - 1)*24
+            draw.text((10, 0 + 200*i), game['name'], font = jaFontSmall, fill = 0)
+            draw.text((width - 10*16, 0 + 200*i), game['updated_at'], font = jaFontSmall, fill = 0)
+            draw.text((firstPosition, 30 + 200*i), game['first_team_name'], font = jaFontSmall, fill = 0)
+            draw.text((lastPosition, 30 + 200*i), game['last_team_name'], font = jaFontSmall, fill = 0)
+            draw.text((width*2/7, 60 + 200*i), str(game['first_run']), font = enFont, fill = 0)
+            draw.text((width/2 + 60, 60 + 200*i), '-', font = enFont, fill = 0)
+            draw.text((width*5/7, 60 + 200*i), str(game['last_run']), font = enFont, fill = 0)
+            if game['winner']:
+                draw.text((firstPosition, 150 + 200*i), '勝:' + game['winner'], font = jaFontSmall, fill = 0)
+            if game['saver']:
+                draw.text((lastPosition, 150 + 200*i), 'S:' + game['saver'], font = jaFontSmall, fill = 0)
+            if game['loser']:
+                draw.text((firstPosition, 175 + 200*i), '敗:' + game['loser'], font = jaFontSmall, fill = 0)
+            if game['holder']:
+                draw.text((lastPosition, 175 + 200*i), 'H:' + game['holder'], font = jaFontSmall, fill = 0)
+            if game['homer']:
+                draw.text((firstPosition, 200 + 200*i), 'HR:' + game['homer'], font = jaFontSmall, fill = 0)
+            draw.line((0, 210 + 200*i , 448, 210 + 200*i), fill = 0)
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(5)  
     
     
     # draw.line((70, 50, 20, 100), fill = 0)
