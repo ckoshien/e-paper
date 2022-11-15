db865978c1c849e273fa1ab6e87a46cc9acb0a36
ckoshien <ckoshien@gmail.com>
2022-11-15 11:13:12 +0900

    no message

--- weather.py
+++ weather.py
@@ -55,10 +55,10 @@ try:
         try:
             icon = Image.open('pic/' + str(data['hourly']['weathercode'][i*2+24]) +'.jpeg')
             icon = icon.resize((40,40))
-            Himage.paste(icon, (31,i*60))
+            Himage.paste(icon, (31,1 + i*60))
             icon2 = Image.open('pic/' + str(data['hourly']['weathercode'][i*2+48]) +'.jpeg')
             icon2 = icon2.resize((40,40))
-            Himage.paste(icon2, (251,i*60))
+            Himage.paste(icon2, (251,1 + i*60))
         except:
             continue
     epd.display(epd.getbuffer(Himage))
